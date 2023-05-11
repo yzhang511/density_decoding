f0030e016303b95a2d1e8fbd3d232bfb1a589359
olivier <olivier.winter@hotmail.fr>
2023-05-11 15:58:41 +0100

    make the repository installable

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ for running this code:
 pip install -r requirements.txt
 pip install -e.
 ```
-also install isosplit [here](https://github.com/cwindolf/isosplit).
+
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 with open("requirements.txt") as f:
-    require = [x.strip() for x in f.readlines() if not x.startswith("git+")]
+    require = [x.strip() for x in f.readlines()]
 
 setup(
     name="clusterless",
     version="0.1",
     packages=["clusterless"],
+    install_requires=require,
 )
