1d5cccaa9e2a1f3fe448352673e12ddce8be1e56
Yizi Zhang <yz4123@columbia.edu>
2023-06-02 06:30:55 -0700

    Update advi.py

--- clusterless/advi.py
+++ clusterless/advi.py
@@ -75,12 +75,15 @@ class ADVI(torch.nn.Module):
         return elbo
     
         
-    def forward(self, y):
+    def forward(self, s, y, ks, ts):
         '''
         Inputs:
         -------
+        s: (n, d) array; n = # of spikes in each batch, d = spike feature dimension.
         y: array of size (k,) if y is binary, and of size (k, t) if y is continuous; 
-           k = # of trials in each batch, t = # of time bins.     
+           k = # of trials in each batch, t = # of time bins. 
+        ks: (n,) index array that denotes the trial each spike belongs to.  
+        ts: (n,) index array that denotes the time bin each spike falls into. 
         '''
         
         # define global variational variables
